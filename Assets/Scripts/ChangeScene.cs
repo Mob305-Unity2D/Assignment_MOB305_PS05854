5a900a1d0525a9aaaae1fa9f78a72c98d2572adf
nguyenducthanhtam <nguyenducthanhtam1@gmail.com>
2018-01-23 01:04:48 +0700

    add project

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void PressChangeScene(string sceneName){
+		Application.LoadLevel(sceneName);
+	}
+}

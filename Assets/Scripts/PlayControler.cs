04fefd2cc94afa0ffc2dd2bc0a2819e59b4a1f92
nguyenducthanhtam <nguyenducthanhtam1@gmail.com>
2018-02-08 11:08:54 +0700

    Making

---
+++ Assets/Scripts/PlayControler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayControler : MonoBehaviour {
+	public float maxSpeed;
+	public float jumpHeight;
+
+	Rigidbody2D myBody;
+	Animator myAnim;
+
+
+
+	// Use this for initialization
+	void Start () {
+		// Mapped
+		myBody = GetComponent<Rigidbody2D>	();
+		myAnim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.getAxis (" ");
+	}
+}
